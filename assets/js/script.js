e3212bbc0fbde0f30c5084abd4bb8eb65938f7a1
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-06-22 15:12:53 -0500

    added new form entry sections and submit actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-var butttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
-butttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
